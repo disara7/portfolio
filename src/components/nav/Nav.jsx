67793880c6a583256c2dc8663c15692ad609be5d
disara7 <129250994+disara7@users.noreply.github.com>
2024-01-14 11:49:18 +0530

    added navigation bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { IoHome } from "react-icons/io5";
+import { FaUserAlt } from "react-icons/fa";
+import { HiMiniCheckBadge } from "react-icons/hi2";
+import { IoMail } from "react-icons/io5";
+import { IoBookmarks } from "react-icons/io5";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><IoHome /></a>
+      <a href="#about"><FaUserAlt /></a>
+      <a href="#experience"><HiMiniCheckBadge /></a>
+      <a href="#services"><IoBookmarks /></a>
+      <a href="#contact"><IoMail /></a>
+    </nav>
   )
 }
 
