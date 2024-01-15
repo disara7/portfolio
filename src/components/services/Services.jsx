95a8290efeeb0ba49ec018d1858f0c9295438271
disara7 <129250994+disara7@users.noreply.github.com>
2024-01-15 16:30:23 +0530

    added awards

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,54 +3,194 @@ import './services.css'
 
 const Services = () => {
   return (
-    <section id='services' className='timestyle'>Awards
-    <div className="timeline">
-<div class="entries">
-  <div class="entry">
-    <div class="title">2011</div>
-    <div class="body">
-      <p>Neque sunt voluptatibus repellat pariatur ut enim. Eveniet rerum suscipit eveniet amet dignissimos. Doloremque et distinctio quod molestiae ut.</p>
-    </div>
-  </div>
-  <div class="entry">
-    <div class="title">2012</div>
-    <div class="body">
-      <p>Quo nobis cumque dolor iure voluptatem voluptatem alias soluta.</p>
-    </div>
-  </div>
-  <div class="entry">
-    <div class="title big">2013</div>
-    <div class="body">
-      <p>Rerum sit libero possimus amet excepturi. Exercitationem enim dolores sunt praesentium dolorum praesentium.</p>
-    </div>
-  </div>
-  <div class="entry">
-    <div class="title">2014</div>
-    <div class="body">
-      <p>Voluptatibus veniam ea reprehenderit atque. Reiciendis non laborum adipisci ipsa pariatur omnis. Sed ipsam repudiandae velit. Omnis libero nostrum aperiam nemo dolor ea eos eius. Esse a non itaque quidem.</p>
-    </div>
-  </div>
-  <div class="entry">
-    <div class="title">2015</div>
-    <div class="body">
-      <p>VAdipisci totam omnis cum et suscipit excepturi et excepturi. Inventore sequi sit ut aliquid. Modi aut dolores dignissimos.</p>
-      <p>Delectus facere officia consequuntur molestias deserunt illo. Placeat laudantium beatae natus excepturi ab nihil voluptates.</p>
-    </div>
-  </div>
-  <div class="entry">
-    <div class="title big">2016</div>
-    <div class="body">
-      <p>Impedit dolorem commodi explicabo fugit aut alias voluptatem. Magnam earum rerum quae dicta quibusdam aliquam ut.</p>
-    </div>
-  </div>
-  <div class="entry">
-    <div class="title">2017</div>
-    <div class="body">
-      <p>Qui facere eos aut suscipit doloremque quos...</p>
-    </div>
-  </div>
-</div>
-</div>
+    <section id='services' className='timestyle'>
+      <h2>Awards</h2>
+
+      <div class="timeline-block timeline-block-left">
+        <div class="marker"></div>
+          <div class="timeline-content">
+            <span>DECEMBER 2023</span>
+            <p><h3>2nd Runners Up - Code With WIE 2023</h3></p>
+            <p>Mine and my friend Minuri Hewage's latest project 'Haven'. Organized by IEEE Sri Lanka Section - Women In Engineering</p>
+          </div>
+      </div>
+
+      <div class="timeline-block timeline-block-right">
+        <div class="marker"></div>
+          <div class="timeline-content">
+            <span>AUGUST 2023</span>
+            <p><h3>1st Runners Up - iX 23 | XR Based Designathon</h3></p>
+            <p>Team Pixel Concepts. 24-Hour designathon held at the IIT premises. Our XR based UI/UX design solution "WellnessGrocers" was highly praised for its realistic-ness.</p>
+          </div>
+      </div>
+
+      <div class="timeline-block timeline-block-left">
+        <div class="marker"></div>
+          <div class="timeline-content">
+            <span>JULY 2023</span>
+            <p><h3>Winner - Lazarus 2.0 Eidos | Ideathon Competition</h3></p>
+            <p>MyJourney (Modern AI-Powered Travel Planning Mobile App) by Team Regex Native (Disara Mapalagama and Minuri Hewage) was awarded the prize for the most outstanding innovative idea at the ideathon organized by IEEE PES - SLIIT</p>
+          </div>
+      </div>
+
+      <div class="timeline-block timeline-block-right">
+        <div class="marker"></div>
+          <div class="timeline-content">
+            <span>MARCH 2023</span>
+            <p><h3>Winner - Lazarus 2.0 Eidos | Ideathon Competition</h3></p>
+            <p>MyJourney (Modern AI-Powered Travel Planning Mobile App) by Team Regex Native (Disara Mapalagama and Minuri Hewage) was awarded the prize for the most outstanding innovative idea at the ideathon organized by IEEE PES - SLIIT</p>
+          </div>
+      </div>
+
+      <div class="timeline-block timeline-block-left">
+        <div class="marker"></div>
+          <div class="timeline-content">
+            <span>MAY 2023</span>
+            <p><h3>2nd Runner Up - HerStage | All Island Competition for Women Entrepreneurs</h3></p>
+            <p>Myself and my friend Minuri Hewage placed third in the all-island competition for women entrepreneurs organized by Youth4SriLanka - AIESEC Sri Lanka, with our business idea "MyJourney - the modern travel planning mobile application".</p>
+          </div>
+      </div>
+
+      <div class="timeline-block timeline-block-right">
+        <div class="marker"></div>
+          <div class="timeline-content">
+            <span>MARCH 2023</span>
+            <p><h3>1st Runner Up - Codecon 3.0 | Inter-University Hackathon</h3></p>
+            <p>Team Regex Native (Disara Mapalagama, Minuri Hewage), with our solution to the economic crisis in Sri Lanka: "MyJourney - the modern travel planning mobile application"</p>
+          </div>
+      </div>
+
+      <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>JANUARY 2023</span>
+         <p><h3>Finalist - IEEE INSL 2022 | Idea Stage</h3></p>
+         <p>MMe and my team AltInnovate made it to the top 10 teams of IEEE INSL 2022 - Idea Stage category with our business idea "StyleUp - Clothing rental mobile application".</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>JANUARY 2023</span>
+         <p><h3>Merit Award - Codefest 2023 | Inter-University Hackathon</h3></p>
+         <p>24-Hour hackathon organized by Faculty of Computing - SLIIT</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>OCTOBER 2022</span>
+         <p><h3>Winner - IEEE INSL '22 Provincial Competition (Sabaragamuwa Province)</h3></p>
+         <p>Organized by IEEE Innovation Nation Sri Lanka. Me and my team AltInnovate became the winner of the provincial competition Sabaragamuwa Province of 
+ IEEE INSL 2022 - Idea Stage category with our business idea "StyleUp - Clothing rental mobile application".</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>JULY 2022</span>
+         <p><h3>Semi Finalists - IDEALIZE 2022 | Inter-University Hackathon</h3></p>
+         <p>StyleUp - Mobile Application by MiNd HaCkErS. Organized by AIESEC in University of Moratuwa</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>MAY 2022</span>
+         <p><h3>2nd Runners Up - HackTron v1.0 – Game Development Bootcamp & Hackathon 2021</h3></p>
+         <p>Shut - PC car simulator game. Organized by IEEE Computer Society of SLTC</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>FEBRUARY 2022</span>
+         <p><h3>Finalists - Designathon 2022 | Codefest Inter-University Hackathon</h3></p>
+         <p>Organized by Department of Computer Science & Software Engineering - SLIIT</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>OCTOBER 2017</span>
+         <p><h3>Finalists - Hack Like a Girl | Women's Hackathon 2022 by SLASSCOM in collaboration with IFC</h3></p>
+         <p>Me and my team MiNd HaCkErS participated and got selected to the final 15 teams with our business idea "MedAura - the pharmacy connecting and organ donor-donee connecting platform"</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>JUNE 2018</span>
+         <p><h3>3rd Place - Conexion '18 | Inter-School ICT Quiz Competition</h3></p>
+         <p>Inter-school ICT Quiz competition organized for the ICT Day of Mahamaya Girls' College, Kandy.</p>
+      </div>
+   </div>
+
+      <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>NOVEMBER 2017</span>
+         <p><h3>
+1st Place - Delito Cibernetico 2017 | Inter-School Web Designing Competition</h3></p>
+         <p>Inter-School Web Designing competition organized for the annual ICT day of Hillwood College Kandy, under the theme "Cybersecurity"</p>
+      </div>
+   </div>
+      
+      <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>OCTOBER 2017</span>
+         <p><h3>1st Place - Evolution Tecnica 17 | Inter-School Web Designing Competition</h3></p>
+         <p>Inter-School Quiz Competition organized for the Annual ICT day of St. Sylvester's College, Kandy.</p>
+      </div>
+   </div>
+
+   <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>OCTOBER 2017</span>
+         <p><h3>1st Place - Evolution Tecninca 17 | Inter-School ICT Quiz Competition</h3></p>
+         <p>Inter-School Quiz Competition organized for the Annual ICT day of St. Sylvester's College, Kandy.</p>
+      </div>
+   </div>
+
+   <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>OCTOBER 2017</span>
+         <p><h3>2nd Place - Sagacious '17 | Inter-School Web Designing Competition</h3></p>
+         
+         <p>Inter-School Web Designing competition organized for the Annual ICT Day of Pushpadana Girls' College, Kandy.</p>
+      </div>
+   </div>
+
+   <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>AUGUST 2017</span>
+         <p><h3>2nd Place - Inter-School Web Designing Competition</h3></p>
+         
+         <p>Inter school web designing competition organized for the annual IT Day of St. Anthony's Girls' College, Kandy.</p>
+      </div>
+   </div>
+
+   <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+      <span>JANUARY 2017</span>
+         <p><h3>1st Round - IT MasterMind Quiz Programme 2017 | Season 02</h3></p>
+         
+         <p>Issued by CSSL and ESOFT Metro Campus. Passed the written test held at ESOFT Metro Campus Kandy, leading the team of three and got selected for the first round of the competition that aired on ITN.</p>
+      </div>
+   </div>
+
 
     </section>
   )
