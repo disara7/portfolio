f6047796c7f146644f63972bcc04436e61a6c2d4
disara7 <129250994+disara7@users.noreply.github.com>
2024-01-15 16:36:44 +0530

    removed testimonials

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,6 +6,7 @@ import { HiMiniCheckBadge } from "react-icons/hi2";
 import { IoMail } from "react-icons/io5";
 import { IoMdTrophy } from "react-icons/io";
 import { useState } from 'react';
+import { RiSlideshow2Fill } from "react-icons/ri";
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
@@ -14,6 +15,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><IoHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAlt /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><HiMiniCheckBadge /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiSlideshow2Fill /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><IoMdTrophy /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><IoMail /></a>
     </nav>
