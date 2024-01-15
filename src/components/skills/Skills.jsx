08d95f7517be9f7b6ab5119c316cba76d6851a68
disara7 <129250994+disara7@users.noreply.github.com>
2024-01-15 16:57:50 +0530

    skill tables added

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import './skills.css'
+
+const Skills = () => {
+  return (
+    <section id='skills'>
+      <h2>Skills</h2>
+      <div className="skill_table">
+        <table>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+            </td>
+          </tr>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+            </td>
+          </tr>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+            </td>
+          </tr>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+            </td>
+          </tr>
+        </table>
+
+        <table>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+            </td>
+          </tr>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+            </td>
+          </tr>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+            </td>
+          </tr>
+          <tr>
+            <td>React JS</td>
+            <td className='skill_box'>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div>
+              <div class="shape shape-square"></div> 
+            </td>
+          </tr>
+        </table>
+      </div>
+
+    </section>
+  )
+}
+
+export default Skills
