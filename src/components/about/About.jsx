c669d6020f50e674cf8685563f8a4f98c108b974
disara7 <129250994+disara7@users.noreply.github.com>
2024-09-26 22:34:13 +0530

    updated about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,14 +13,14 @@ const About = () => {
           </h3>
           <p className='about__style'>
           I am a 24 year old Software Engineering undergraduate who started my coding journey when I was 16. I have always wanted to think differently and apply my knowledge to give creative solutions to problems. </p>
-          <h4 className='about__style'>Education</h4>
+          <h4 className='about__style'>Education:</h4>
           <p className='about__style'>
             <ul className='educard'>
               <li>BSc. (Hons) in Software Engineering - Sabaragamuwa University of Sri Lanka</li> <br />
               <li>Diploma in Business Management - ESOFT Metro Campus, Kandy</li> <br />
               <li>GCE (A/L) ICT, Combined Mathematics, Physics - Girls' High School, Kandy</li>
             </ul>
-         
+            <br />
           </p>
           <div className="cardset">
           <div class="card">
