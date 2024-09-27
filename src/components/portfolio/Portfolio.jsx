c68668c9674512575dd64589dd1d8dea56766807
disara7 <129250994+disara7@users.noreply.github.com>
2024-09-27 09:40:32 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -37,11 +37,25 @@ const Portfolio = () => {
               <img src={p2} alt="Mobile App Project 2" />
               <h4>ShopSense - Online Clothing Shopping for Blind People</h4>
               <p>“ShopSense” provides online shopping accessibility for visually impaired people.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
             <div className='carousel'>
               <img src={p3} alt="Mobile App Project 3" />
               <h4>Learning Management System</h4>
               <p>An interactive LMS for educational institutions.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
           </Carousel>
         </div>
@@ -54,16 +68,37 @@ const Portfolio = () => {
               <img src={p1} alt="Web App Project 1" />
               <h4>UniStore - Online Store for Preloved University Essentials</h4>
               <p>An e-commerce platform for university students to buy and sell second-hand items.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
             <div className='carousel'>
               <img src={p2} alt="Web App Project 2" />
               <h4>My Portfolio</h4>
               <p>A personal portfolio showcasing various web development projects.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
             <div className='carousel'>
               <img src={p3} alt="Web App Project 3" />
               <h4>IEEE WIE Sri Lanka Section - Website Redesign</h4>
               <p>A redesigned website for the IEEE WIE Sri Lanka Section.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
           </Carousel>
         </div>
@@ -76,21 +111,49 @@ const Portfolio = () => {
               <img src={p1} alt="UI/UX Case Study 1" />
               <h4>Trophy - Employee Loyalty Rewarding System</h4>
               <p>A UI/UX case study on the Trophy loyalty rewarding system.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
             <div className='carousel'>
               <img src={p2} alt="UI/UX Case Study 2" />
               <h4>Sentry - A Solution to Human-Elephant Conflicts</h4>
               <p>A solution to mitigate human-elephant conflicts through a mobile app.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
             <div className='carousel'>
               <img src={p3} alt="UI/UX Case Study 3" />
               <h4>ResQ - Disaster Management System</h4>
               <p>An interactive system to manage disaster responses efficiently.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
             <div className='carousel'>
               <img src={p3} alt="UI/UX Case Study 4" />
               <h4>Watts - Gamified Power Saving</h4>
               <p>A gamified mobile app for saving electricity through engaging features.</p>
+              <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div>
+                
+              </div>
             </div>
           </Carousel>
         </div>
