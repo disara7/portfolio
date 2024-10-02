044966de05fa73f99412ad5c6a0d3e749befa18f
disara7 <129250994+disara7@users.noreply.github.com>
2024-10-02 09:51:55 +0530

    added form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,19 @@ const Contact = () => {
   return (
     <section id='contact'>
       <h2 className='section__title'>Contact Me</h2>
+      <div className='contactform' id="form">
+							<form action="" id="contactForm" method="post">
+								<span>Name</span>
+								<input type="text" name="name" class="name" placeholder="Enter your name" /><br />
+								<span>Email</span>
+								<input type="text" name="email" class="email" placeholder="Enter your email" /><br />
+								<span id="captcha"></span>
+								
+								<span>Message</span>
+								<textarea class="message" placeholder="Enter your message" ></textarea><br />
+								<input type="submit" name="submit" value="Send e-mail" class="submit" ></input>
+							</form>
+						</div>
     </section>
   )
 }
