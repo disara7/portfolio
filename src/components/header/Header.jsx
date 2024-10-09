a4aa8daa3d60a3177ec017fc1dd1ab191430baf2
disara7 <129250994+disara7@users.noreply.github.com>
2024-10-09 17:26:47 +0530

    made responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
       <div className="container header__container">
         <h1 className='fname'>DISARA</h1>
         <h5 className='name__post'>Full Stack Developer</h5>
-        <h5 className="text-light name__description">I am a multidisciplinary software engineering undergraduate with a creative problem-solving approach and an artistic mindset. I have led successful national-level award winning hackathon projects, combining technical skills and design thinking to deliver impactful solutions. Passionate about building efficient, user-centred systems, I thrive in collaborative environments and enjoy tackling real-world challenges.</h5>
-        <CV />
+        <h5 className="text-light name__description">I am a multidisciplinary software engineering undergraduate with a creative problem-solving approach and an artistic mindset.</h5>
+        {/* <CV /> */}
         <Socials />
 
         <div className="me">
