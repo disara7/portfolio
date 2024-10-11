fd2f86393def2243e2d114df39ec7c42806279d8
disara7 <129250994+disara7@users.noreply.github.com>
2024-10-11 14:59:21 +0530

    updated projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -15,6 +15,57 @@ const Portfolio = () => {
     <section id='portfolio'>
       <h2 className='section__title'>Portfolio</h2>
       <div className="portfolio_content">
+      <h3>Mobile Applications</h3>
+      <div className="projects">
+      <div className="portfolio_card">
+          <img src={p1} alt="Web App Project 1" />
+          <h4>Trophy - Employee Loyalty Rewarding System</h4>
+          <p>“Trophy” is a non-monetary token-based loyalty rewarding system to improve a company’s employees’ work experience.</p>
+          <div className="links">
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div> 
+          </div>
+        </div>
+        <div className="portfolio_card">
+          <img src={p1} alt="Web App Project 1" />
+          <h4>ShopSense - Online Clothing Shopping for Blind People</h4>
+          <p>“ShopSense” provides online shopping accessibility for visually impaired people.</p>
+          <div className="links">
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div> 
+          </div>
+        </div>
+        <div className="portfolio_card">
+          <img src={p1} alt="Web App Project 1" />
+          <h4>Learning Management System</h4>
+          <p>An interactive LMS for educational institutions.</p>
+          <div className="links">
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div> 
+          </div>
+        </div>
+        
+      </div>
+      <div className="portfolio_card">
+          <img src={p1} alt="Web App Project 1" />
+          <h4>MYJOURNEY - AI Based Travel Planning</h4>
+          <p>An interactive LMS for educational institutions.</p>
+          <div className="links">
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+              <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div> 
+          </div>
+        </div>
+        <br />
+        <br />
+        
         <h3>Web Applications</h3>
         <div className="projects">
           <div className="portfolio_card">
@@ -29,15 +80,27 @@ const Portfolio = () => {
             </div>
           </div>
           <div className="portfolio_card">
-
+          <img src={p1} alt="Web App Project 1" />
+            <h4>IEEE WIE Sri Lanka Section - Website Redesign</h4>
+            <p>A redesigned website for the IEEE WIE Sri Lanka Section.</p>
+            <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div> 
+            </div>
           </div>
           <div className="portfolio_card">
-
+          <img src={p1} alt="Web App Project 1" />
+            <h4>UniStore - Online Store for Preloved University Essentials</h4>
+            <p>An e-commerce platform for university students to buy and sell second-hand items.</p>
+            <div className="links">
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BsGithub /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><BiLogoAdobe /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><FaMedium /></a></div>
+                <div className="relatedlinks"><a className='social__icons' href="https://github.com/disara7" target='_blank'><MdVideoLibrary /></a></div> 
+            </div>
           </div>
-        </div>
-        <h3>Mobile Applications</h3>
-        <div className="portfolio_card">
-
         </div>
         <h3>UI/UX Research Case Studies</h3>
         <div className="portfolio_card">
