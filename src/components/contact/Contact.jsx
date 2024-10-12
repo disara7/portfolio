1d31b241f82fce39cf27ca0c2e0295d7fd336d5e
disara7 <129250994+disara7@users.noreply.github.com>
2024-10-12 16:48:26 +0530

    added contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,19 +4,28 @@ import './contact.css'
 const Contact = () => {
   return (
     <section id='contact'>
-      <h2 className='section__title'>Contact Me</h2>
-      <div className='contactform' id="form">
-			<form action="" id="contactForm" method="post">
-				<span>Name</span>
-				<input className='field' type="text" name="name" class="name" placeholder="Enter your name" /><br />
-				<span>Email</span>
-				<input className='field' type="text" name="email" class="email" placeholder="Enter your email" /><br />
-				<span id="captcha"></span>
-				<span>Message</span>
-				<textarea className='field' class="message" placeholder="Enter your message" ></textarea><br />
-				<input type="submit" name="submit" value="Send e-mail" class="submit" ></input>
-			</form>
+      <div className="contact">
+		<br />
+	  <h2 className='section__title'>Contact Me</h2>
+	  <br />
+	  <div className='contactform' id="form">
+	  <form action="" id="contactForm" method="post">
+    <span>Name</span>
+    <input class="field" type="text" name="name" placeholder="Enter your name" /><br />
+    
+    <span>Email</span>
+    <input class="field" type="email" name="email" placeholder="Enter your email" /><br />
+    
+    <span id="captcha"></span>
+    
+    <span>Message</span>
+    <textarea class="field" name="message" placeholder="Enter your message"></textarea><br />
+    
+    <input type="submit" name="submit" value="Send e-mail" class="submit" />
+</form>
+
 		</div>
+	  </div>
     </section>
   )
 }
