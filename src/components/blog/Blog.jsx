1594aeb5dc2d535d4dcfe782adc35b437c8c7ad4
disara7 <129250994+disara7@users.noreply.github.com>
2024-11-10 17:31:31 +0530

    added blog content

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -12,6 +12,16 @@ const Blog = () => {
       <p>“Trophy” is a non-monetary token-based loyalty mobile application solution to maintain your company’s employees’ sense of community and improve the team member experience in the office with more interactivity, inclusivity, and motivation.</p>
       <a href="https://medium.com/@disaradm07/trophy-employee-loyalty-app-ui-ux-case-study-32f781794a92">READ FULL ARTICLE ON MEDIUM</a>
     </div>
+    <div className="blogcard">
+      <h1>Can We Use AI Personas for User Testing?</h1>
+      <p>As funny as it sounds, using AI personas instead of actual users for user testing can seem like an absurd idea. Since software is mainly meant to solve an existing issue, it’s created for actual, existing users. But there could be imaginary problems that software solutions can solve…</p>
+      <a href="https://medium.com/@disaradm07/can-we-use-ai-personas-for-user-testing-8cfe64d1f0f7">READ FULL ARTICLE ON MEDIUM</a>
+    </div>
+    <div className="blogcard">
+      <h1>“Watts”: Gamified Power Saving Mobile App | UI/UX Case Study</h1>
+      <p>“Watts” is a mobile application that tracks electricity usage via smart energy meters in the household and aims to promote energy conservation and economic growth within individuals.</p>
+      <a href="https://medium.com/@disaradm07/watts-gamified-power-saving-mobile-app-ui-ux-case-study-f3f4c57db871">READ FULL ARTICLE ON MEDIUM</a>
+    </div>
 	  
 
 	  </div>
