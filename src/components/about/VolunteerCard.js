9ed9e1df73cf606701617fee6a3fc98cdd7c3efc
disara7 <129250994+disara7@users.noreply.github.com>
2024-11-12 11:45:18 +0530

    Update VolunteerCard.js

--- src/components/about/VolunteerCard.js
+++ src/components/about/VolunteerCard.js
@@ -1,13 +1,34 @@
 // VolunteerCard.js
 import React from 'react';
+import Slider from 'react-slick';
+import './volunteerCard.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
+const VolunteerCard = ({ title, image, description, images }) => {
+  const carouselSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-const VolunteerCard = ({ title, image, description }) => {
   return (
     <div className="voluncard fade-in">
-      <img src={image} alt={title} className="voluncard__image" />
-      <h4 className="voluncard__title">{title}</h4>
-      <p className="voluncard__description">{description}</p>
+      <div className="voluncard__content">
+        <h3 className="voluncard__title">{title}</h3>
+        <p className="voluncard__description">{description}</p>
+      </div>
+      <div className="voluncard__carousel">
+        <Slider {...carouselSettings}>
+          {images.map((img, index) => (
+            <div key={index}>
+              <img src={img} alt={`${title} ${index + 1}`} className="voluncard__carousel-image" />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
