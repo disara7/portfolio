b7100f5408cb457c5fd9e130cfc43b8c0076ccf8
disara7 <129250994+disara7@users.noreply.github.com>
2026-08-03 16:14:43 +0530

    about-section: updated

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -3,8 +3,14 @@ export const ABOUT = {
   name: 'Disara Mapalagama',
 
   description: [
-    'I am a 24-year-old Software Engineering undergraduate who started my coding journey when I was 16. I have always wanted to think differently and apply my knowledge to give creative solutions to problems.',
+    'I am a 26-year-old Software Engineer who started my coding journey when I was 16. I have always wanted to think differently and apply my knowledge to give creative solutions to problems.',
     'I have a creative problem-solving approach, an artistic mindset, and a proven ability to learn quickly. I have led award-winning hackathon projects, combining technical skills and design thinking. Passionate about efficient, user-focused systems, I excel in teamwork and solving real-world challenges.',
+  ],
+
+  experienceTitle: 'Experience:',
+  experience: [
+    'Associate Software Engineer',
+    'Software Engineer Intern',
   ],
 
   educationTitle: 'Education:',
