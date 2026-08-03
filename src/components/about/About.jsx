3854d54815031d32687b79beff59bf42ff0d49d5
disara7 <129250994+disara7@users.noreply.github.com>
2026-08-03 17:59:56 +0530

    Revert "about-section: updated"
    This reverts commit 485e4d3334d13d547957d38b771e467c4ac39a0b.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -66,14 +66,7 @@ const About = () => {
             <h3 className='about__style fullname fade-in'>
               {ABOUT.name}
             </h3>
-            <div className="about-description fade-in">
-              <p className="about__style">
-                {ABOUT.description[0]}
-                <br />
-                <br />
-                {ABOUT.description[1]}
-              </p>
-            </div>
+            <p className='about__style fade-in'>{ABOUT.description[0]}<br/><br/>{ABOUT.description[1]}</p>
             <br />
             <h4 className='about__style fade-in'>{ABOUT.experienceTitle}</h4>
             <ul className='experience fade-in about__style'>
