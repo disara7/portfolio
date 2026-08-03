98bf8c7e566933cdc8ccf8b20026b826846718d1
disara7 <129250994+disara7@users.noreply.github.com>
2026-08-03 15:58:40 +0530

    moved strings to app constants

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,6 +15,7 @@ import XTREME from '../../assets/volunteering_logos/xtreme.png';
 import SLSAC from '../../assets/volunteering_logos/slsac.png';
 import AIESEC from '../../assets/volunteering_logos/aiesec.png';
 import blank from '../../assets/blank.png';
+import { ABOUT } from '../../constants/appConstants';
 
 const About = () => {
   const [popupData, setPopupData] = useState(null);
@@ -59,39 +60,33 @@ const About = () => {
   return (
     <section id='about'>
       <div className="about fade-in">
-        <h2 className='section__title fade-in'>About Me</h2><br />
+        <h2 className='section__title fade-in'>{ABOUT.sectionTitle}</h2>
         <div className="container about__container fade-in">
           <div className="about__content fade-in">
             <h3 className='about__style fullname fade-in'>
-              Disara Mapalagama
+              {ABOUT.name}
             </h3>
-            <p className='about__style fade-in'>
-              I am a 24-year-old Software Engineering undergraduate who started my coding journey when I was 16. I have always wanted to think differently and apply my knowledge to give creative solutions to problems.
-              <br /><br />I have a creative problem-solving approach, an artistic mindset, and a proven ability to learn quickly. I have led award-winning hackathon projects, combining technical skills and design thinking. Passionate about efficient, user-focused systems, I excel in teamwork and solving real-world challenges.
-            </p>
+            <p className='about__style fade-in'>{ABOUT.description[0]}<br/><br/>{ABOUT.description[1]}</p>
             <br />
-            <h4 className='about__style fade-in'>Education:</h4>
+            <h4 className='about__style fade-in'>{ABOUT.educationTitle}</h4>
             <ul className='educard fade-in about__style'>
-              <li>BSc. (Hons) in Software Engineering - Sabaragamuwa University of Sri Lanka</li>
-              <br /><li>Diploma in Business Management - ESOFT Metro Campus, Kandy</li>
-              <br /><li>GCE (A/L) ICT, Combined Mathematics, Physics - Girls' High School, Kandy</li>
+              {ABOUT.education.map((item, index) => (
+                <React.Fragment key={index}>
+                  <li>{item}</li>
+                  {index < ABOUT.education.length - 1 && <br />}
+                </React.Fragment>
+              ))}
             </ul>
             <br />
             <div className="cardsett fade-in">
-              <div className="cardd">
-                <h2 className='number'>8+</h2>
-                <p className='number_description'>Years of Experience</p>
-              </div>
-              <div className="cardd">
-                <h2 className='number'>15+</h2>
-                <p className='number_description'>Projects</p>
-              </div>
-              <div className="cardd">
-                <h2 className='number'>25+</h2>
-                <p className='number_description'>Awards and Honors</p>
-              </div>
+              {ABOUT.stats.map((stat) => (
+                <div className="cardd" key={stat.label}>
+                  <h2 className="number">{stat.value}</h2>
+                  <p className="number_description">{stat.label}</p>
+                </div>
+              ))}
             </div>
-            <h4 className='about__style fade-in volunteering'>Volunteering:</h4>
+            <h4 className='about__style fade-in volunteering'>{ABOUT.volunteeringTitle}</h4>
             <div className="volunlogos">
               {volunteerData.map((volunteer, index) => (
                 <div
